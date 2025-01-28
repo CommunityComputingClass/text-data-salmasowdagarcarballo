e329139a8ea1ee96e0c36fca20573151f27cd402
salmasowdagarcarballo <26salma.carballo@lwhs.org>
2025-01-28 23:21:37 +0000

    submitting my first code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,21 @@
-function setup() {
-  createCanvas(400, 400);
+let myMessage = "Good Evening"
+let xLocation = 100;
+let myFont;
+
+function preload(){
+  myFont = loadFont("VIRUST.ttf")
 }
 
+function setup() {
+  createCanvas(400, 400);
+
+}
 function draw() {
-  background(200);
-  
+  background(9, 93, 145);
+noStroke();
+fill("white");
+textSize(30)
+textFont(myFont)
+text(myMessage,xLocation,200);
+xLocation = xLocation
 }
