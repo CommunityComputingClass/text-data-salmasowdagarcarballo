ce2104917baeb8043b9d8d519c56d8d865369301
salmasowdagarcarballo <26salma.carballo@lwhs.org>
2025-02-04 19:56:46 +0000

    submitting final questionnaire

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,6 +1,7 @@
 let myMessage = "Who is your favorite music artist?"
 let contents = "";
 let Button;
+let textBox; 
 let xLocation = 15;
 let myFont;
 
@@ -13,6 +14,12 @@ function setup() {
   Button = createButton("show");
   Button.position(175,150)
   Button.mousePressed(showText)
+
+  textBox = createInput("Type your answer here")
+  textBox.size(250,20)
+  textBox.position(50,200)
+  textBox.input(storeText)
+
   background(245, 51, 87);
 }
 function draw() {
@@ -28,5 +35,9 @@ function keyTyped(){
   contents = contents + key;
 }
 function showText(){
-  text(contents,50,250,400,200);
+  text(contents,50,250,400,200)
+  textBox.hide();
+}
+function storeText(){
+  contents = this.value();
 }
